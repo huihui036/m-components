42218ffe91108d7d226cfa841db99f63165fcc41
huihui <1315574336@qq.com>
2025-08-17 15:57:34 +0800

    删除helloWOED

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,19 +18,7 @@ const count = ref(0)
 
   <p>See <code>README.md</code> for more information.</p>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+ 
 </template>
 
 <style scoped>
